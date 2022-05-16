c203b5b0f3aa9232504cdd8d37ae82557a33d402
havesga86 <hernan.vesga.rincon@gmail.com>
2022-05-16 16:56:05 -0500

    AJAX agregado
    Modificaciones de 16/05/2022, finalizando AJAX

---
+++ ajax.js
@@ -0,0 +1,26 @@
+
+(function() {
+    $("#curl").val("" + window.location.href);
+  })();
+  
+  function getcontent() {
+    $("#estado").css("background-color", "aquamarine");
+    $("#estado").html('Cargando');
+   
+    setTimeout(funcAjax, 2000);
+  
+    function funcAjax() {
+      $.ajax({
+        url: window.location.href + 'datos.txt',
+        async: false,
+        success: function(data,status, xhr) {
+          let headers = xhr.getAllResponseHeaders();
+          $('#contenidos').append(data);
+          $("#estado").css("background-color", "aquamarine");
+          $("#estado").html('Completado');
+          $("#headers").append(headers);
+          $("#state").val(xhr.status +', '+ xhr.statusText);
+        }
+      });
+    }
+  }
